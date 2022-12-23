12af696b1b7f9e7225161430f0fc740d4b913cd3
Piyush <impiyush016@gmail.com>
2022-12-23 14:56:38 +0530

    Improved logic to show board

--- project/game.py
+++ project/game.py
@@ -10,10 +10,11 @@ just_fix_windows_console()
 
 
 class Board:
-    bomb = "\033[33m*\033[39m"
-
     def __init__(self, dim_row, dim_col, num_mines) -> None:
         self.board = [[None for _ in range(dim_col)] for __ in range(dim_row)]
+        self.player_board = [
+            ["#" for _ in range(dim_col)] for __ in range(dim_row)
+        ]  # Board shown to player
         self.dim_row = dim_row  # row dimension
         self.dim_col = dim_col  # col dimension
         self.num_mines = num_mines  # number of mines
@@ -27,14 +28,14 @@ class Board:
         # Set every other cell with number of surrounding mines
         for row in range(self.dim_row):
             for col in range(self.dim_col):
-                if self.board[row][col] == Board.bomb:
+                if self.board[row][col] == "*":
                     continue
                 self.board[row][col] = self.get_surrounding_mines(row, col)
 
     def __str__(self) -> str:
         # return f"{self.player_bd()}"
         return tabulate(
-            self.player_bd(),
+            self.player_board,
             headers=range(self.dim_col),
             showindex=True,
             tablefmt="outline",
@@ -49,11 +50,11 @@ class Board:
             row = random.randint(0, self.dim_row - 1)
             col = random.randint(0, self.dim_col - 1)
             # if already planted at that location, conitnue
-            if self.board[row][col] == Board.bomb:
+            if self.board[row][col] == "*":
                 continue
 
             # Plant mine
-            self.board[row][col] = Board.bomb
+            self.board[row][col] = "*"
             mines_planted += 1
 
     def get_surrounding_mines(self, row, col) -> int:
@@ -63,17 +64,19 @@ class Board:
         mine_count = 0
         for r in range(max(0, row - 1), min(row + 2, self.dim_row)):
             for c in range(max(0, col - 1), min(col + 2, self.dim_col)):
-                if self.board[r][c] == Board.bomb:
+                if self.board[r][c] == "*":
                     mine_count += 1
-        return self.colored(mine_count)
+        return mine_count
 
-    def colored(self, cnt) -> str:
-        """Return colored str value of given integer"""
-        if cnt == 0:
+    def colored(self, c) -> str:
+        """Return colored str value of given character"""
+        if c == "*":
+            return f"\033[33m{c}\033[39m"
+        if c == 0:
             return "\033[32m0\033[39m"
-        if cnt in [1, 2]:
-            return f"\033[34m{cnt}\033[39m"
-        return f"\033[31m{cnt}\033[39m"
+        if c in [1, 2]:
+            return f"\033[34m{c}\033[39m"
+        return f"\033[31m{c}\033[39m"
 
     def dig(self, row, col) -> bool:
         """Uncover cell at give row, col"""
@@ -81,10 +84,11 @@ class Board:
         self.visited.add((row, col))
 
         # If mine: return false
-        if self.board[row][col] == Board.bomb:
+        if self.board[row][col] == "*":
             return False
 
-        if self.board[row][col] != "\033[32m0\033[39m":
+        if self.board[row][col] > 0:
+            self.player_board[row][col] = self.colored(self.board[row][col])
             return True
         # If number of surrounding mines is 0: recursively open all surrounding cells
         for r in range(max(0, row - 1), min(row + 2, self.dim_row)):
@@ -94,15 +98,11 @@ class Board:
                 self.dig(r, c)
         return True
 
-    def player_bd(self):
-        """Return board meant for players to see"""
-        return [
-            [
-                self.board[row][col] if (row, col) in self.visited else "#"
-                for col in range(self.dim_col)
-            ]
-            for row in range(self.dim_row)
-        ]
+    def uncover_all(self) -> None:
+        """Show complete board to the player"""
+        for r in range(self.dim_row):
+            for c in range(self.dim_col):
+                self.player_board[r][c] = self.colored(self.board[r][c])
 
 
 def play(dim_row, dim_col, num_mines):
@@ -118,24 +118,25 @@ def play(dim_row, dim_col, num_mines):
             re.split(
                 r",\s*",
                 input(
-                    f"Enter the cell location as below pattern\nrow(0 - {dim_row - 1}), col(0 - {dim_col - 1}): "
+                    f"Enter the cell location row in range[0 - {dim_row - 1}], col in range[0 - {dim_col - 1}]\nrow,col: "
                 ).strip(),
             ),
         )
         res = board.dig(row, col)  # Dig at that location
         # If location is bomb, display entire board and game over
         if not res:
-            board.visited = [(r, c) for c in range(dim_col) for r in range(dim_row)]
+            board.uncover_all()
             print(board)
             sys.exit("GAME OVER !!!")
 
     print()
-    board.visited = [(r, c) for r in range(dim_row) for c in range(dim_col)]
+    board.uncover_all()
     print(board)
     print("CONGRATULATIONS!! You Won.")
 
 
-play(2, 2, 1)
+if __name__ == "__main__":
+    play(3, 3, 2)
 # b = Board(8, 8, 10)
 # b.make_board()
 # print(b)
